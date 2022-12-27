63c34091fa9ded54123797bc87d4db2e22565d81
subodh.dere <subodh.dere.7@gmail.com>
2022-12-27 21:05:58 +0530

    push kasm logs to cloudwatch

---
+++ cwagent/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+locals {
+  userdata = templatefile("user_data.sh", {
+    ssm_cloudwatch_config = aws_ssm_parameter.cw_agent.name
+  })
+}
+
+resource "aws_instance" "this" {
+  ami                  = "ami-0cca134ec43cf708f"
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.this.name
+  user_data            = local.userdata
+  tags                 = { Name = "EC2-with-cw-agent" }
+}
+
+resource "aws_ssm_parameter" "cw_agent" {
+  description = "Cloudwatch agent config to configure custom log"
+  name        = "/cloudwatch-agent/config"
+  type        = "String"
+  value       = file("cw_agent_config.json")
+}
