3a54788b2f9ddde613fcb81cf204a944babbcb02
Subodh <32792363+subodhdere@users.noreply.github.com>
2023-01-19 20:27:55 +0530

    Update user_data.sh

--- cwagent/user_data.sh
+++ cwagent/user_data.sh
@@ -5,12 +5,15 @@ set -e
 exec > >(tee /var/log/user-data.log|logger -t user-data-extra -s 2>/dev/console) 2>&1
 
 # Make sure we have all the latest updates when we launch this instance
-yum update -y
-yum upgrade -y
+apt update -y
+apt upgrade -y
 
 # Configure Cloudwatch agent
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-rpm -U ./amazon-cloudwatch-agent.rpm
+curl -o /root/amazon-cloudwatch-agent.deb https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
+dpkg -i -E /root/amazon-cloudwatch-agent.deb
+usermod -aG adm cwagent
+systemctl enable amazon-cloudwatch-agent.service
+service amazon-cloudwatch-agent start
 
 # Use cloudwatch config from SSM
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
